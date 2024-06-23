21572fc96c25682fc451e65c7a6b6e8c1a7d8706
Nikolay Miroshnichenko <3dutka@gmail.com>
2024-06-23 21:53:13 +0200

    edit zone name

--- server/scripts/web_scraping.py
+++ server/scripts/web_scraping.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from bs4 import BeautifulSoup
 
-import chromedriver_autoinstaller
+# import chromedriver_autoinstaller
 import time
-import os
+# import os
 
 
 def terror_zone_loop():
@@ -17,15 +17,16 @@ def terror_zone_loop():
 
         if current_time.tm_min == 5 or first_run:
 
-            chromedriver_autoinstaller.install()
+            # chromedriver_autoinstaller.install()
 
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--no-sandbox")
+            # chrome_options = webdriver.ChromeOptions()
+            # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            # chrome_options.add_argument("--headless")
+            # chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--no-sandbox")
 
-            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+            # driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+            driver = webdriver.Chrome()
 
             driver.get("https://www.d2emu.com/tz")
 
