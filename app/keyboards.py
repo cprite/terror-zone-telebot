0edc52c0c5cfbf040e98e5322d0511090469c93b
Nikolay Miroshnichenko <3dutka@gmail.com>
2024-07-18 21:34:51 +0200

    minor bug fix

--- app/keyboards.py
+++ app/keyboards.py
@@ -16,7 +16,7 @@ async def menu(language, user_id):
     menu_keyboard.add(InlineKeyboardButton(text=language['menu'][3], callback_data="current_zone"))
     menu_keyboard.add(InlineKeyboardButton(text=language['menu'][4], callback_data="language"))
 
-    if user_id == 1132338630:
+    if user_id == 1132338630 or user_id == 246245209:
         menu_keyboard.add(InlineKeyboardButton(text="Панель админа", callback_data="admin"))
 
     return menu_keyboard.adjust(2).as_markup()
