46ceee2cc68ffbb882f4b14076801b9f79e39c7a
Nikolay Miroshnichenko <3dutka@gmail.com>
2024-07-18 17:30:13 +0200

    minor bug fix

--- server/scripts/web_scraping.py
+++ server/scripts/web_scraping.py
@@ -40,8 +40,6 @@ def terror_zone_loop():
 
             span = soup.find_all("span", {"class": "terrorzone darkmode-ignore"})
 
-            print(span)
-
             current = span[0]
             next = span[1]
 
