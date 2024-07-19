85f91ada91d0f78b69cdf5ce50acc0a17d54680b
Nikolay Miroshnichenko <3dutka@gmail.com>
2024-07-19 12:34:58 +0200

    add maintenace mode to temporary prevent users using app

--- app/admin/stats/csv/users.py
+++ app/admin/stats/csv/users.py
@@ -8,50 +8,50 @@ def add_new_user(id):
 
     if id not in get_users():
 
-        df = pd.read_csv(os.path.join("app/database/csv", "users.csv"))
+        df = pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))
 
         df = df._append(pd.DataFrame({"user": [id],
                                       "looping": [0],
                                       "all_zones": [0]}), ignore_index=True)
 
-        df.to_csv(os.path.join("app/database/csv", "users.csv"), index=False)
+        df.to_csv(os.path.join("app/admin/stats/csv", "users.csv"), index=False)
 
 
 def get_users():
-    return pd.read_csv(os.path.join("app/database/csv", "users.csv"))["user"].tolist()
+    return pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))["user"].tolist()
 
 
 def delete_user(id):
-    df = pd.read_csv(os.path.join("app/database/csv", "users.csv"))
+    df = pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))
     df = df[df["user"] != id]
-    df.to_csv(os.path.join("app/database/csv", "users.csv"), index=False)
+    df.to_csv(os.path.join("app/admin/stats/csv", "users.csv"), index=False)
 
 
 
 # SWITCHES FOR STATS DATA
 def switch_looping(id):
-    df = pd.read_csv(os.path.join("app/database/csv", "users.csv"))
+    df = pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))
 
     if df.loc[df["user"] == id, "looping"].item() == 0:
         df.loc[df["user"] == id, "looping"] = 1
     else:
         df.loc[df["user"] == id, "looping"] = 0
 
-    df.to_csv(os.path.join("app/database/csv", "users.csv"), index=False)
+    df.to_csv(os.path.join("app/admin/stats/csv", "users.csv"), index=False)
 
 
 def switch_all_zones(id):
-    df = pd.read_csv(os.path.join("app/database/csv", "users.csv"))
+    df = pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))
 
     if df.loc[df["user"] == id, "all_zones"].item() == 0:
         df.loc[df["user"] == id, "all_zones"] = 1
     else:
         df.loc[df["user"] == id, "all_zones"] = 0
 
-    df.to_csv(os.path.join("app/database/csv", "users.csv"), index=False)
+    df.to_csv(os.path.join("app/admin/stats/csv", "users.csv"), index=False)
 
 
 # STATS FOR PANEL
 def get_stats():
-    df = pd.read_csv(os.path.join("app/database/csv", "users.csv"))
+    df = pd.read_csv(os.path.join("app/admin/stats/csv", "users.csv"))
     return df["looping"].sum(), df["all_zones"].sum()
